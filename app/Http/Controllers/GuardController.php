b6c8a19470622b579f89e1378cbf50efc14a91de
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-04-12 13:53:34 +0700

    add: data guard validation

--- app/Http/Controllers/GuardController.php
+++ app/Http/Controllers/GuardController.php
@@ -5,31 +5,87 @@ namespace App\Http\Controllers;
 use App\Models\Guard;
 use Illuminate\Http\Request;
 
-class GuardController extends Controller {
-    
-    public function showGuard() {
+class GuardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() {
         $guards = Guard::all();
-        return view('guard.guard', compact('guards'));
+        return view("guard.guard", compact("guards"));
     }
 
-    public function formInsert() {
-        return view('guard.insert');
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {
+        return view("guard.create");
     }
 
-    public function insert(Request $request) {
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {
+        // $validate = $request->validate([
+        //     'fullname' => 'required|max:150',
+        //     'email' => 'required',
+        //     'phone_number' => 'required',
+        //     'birth_date' => 'required',
+        // ]);
+        // Guard::create($validate);
 
+        $validatedData = $request->validate([
+            'fullname' => 'required',
+            'birth_date' => 'required',
+            'email' => 'required|email|unique:guards,email',
+            'password' => 'required|min:6',
+            'phone_number' => 'required',
+            'address' => 'required',
+        ], [
+            'fullname.required' => 'Kolom Nama harus diisi',
+            'birth_date.required' => 'Tanggal Lahir harus diisi',
+            'email.required' => 'Kolom Email harus diisi',
+            'email.email' => 'Format Email tidak valid',
+            'email.unique' => 'Email sudah digunakan',
+            'password.required' => 'Kolom Password harus diisi',
+            'password.min' => 'Password harus memiliki minimal 6 karakter',
+            'phone_number.required' => 'Kolom Nomor Telepon harus diisi',
+            'address.required' => 'Kolom Alamat harus diisi',
+        ]);
+        
+        Guard::create($validatedData);
+        return redirect('/guard')->with('success','Berhasil ditambahkan!');
+        }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Guard $guard)
+    {
+        //
     }
 
-    public function formupdate(){
-
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Guard $guard)
+    {
+        //
     }
 
-    public function update() {
-
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Guard $guard)
+    {
+        //
     }
 
-    public function delete() {
-
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Guard $guard)
+    {
+        //
     }
-    
 }
--- app/Models/Guard.php
+++ app/Models/Guard.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guard extends Model {
     use HasFactory;
-
-    protected $table = 'guards';
+    
+    protected $table = "guards";
     protected $guarded = ['id'];
-    protected $fillable = [
-        'nik', 'fullname_guard', 'birth_date', 'email_guard',
-        'password_guard', 'phone_number', 'address', 'photo',
-    ];
+    // protected $fillable = [
+    //     'nik','fullname_guard','birth_date','email_guard',
+    //     'password_guard','phone_number','address','photo'
+    // ];
+
     public $timestamps = false;
 }
