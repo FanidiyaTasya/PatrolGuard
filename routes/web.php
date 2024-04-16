f00870635fbf78b41fcc49fa113c5c6a1f6aaa51
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-04-16 19:09:02 +0700

    style: add for alert

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,23 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GuardController;
 use App\Http\Controllers\ScheduleController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get("/", [AuthController::class, 'signin']);
+Route::get("/", [AuthController::class, 'signin'])->name('sign-in');
 Route::post("/", [AuthController::class, 'authenticate']);
+Route::post("/logout", [AuthController::class, 'logout']);
 
 Route::get('/sign-up', function () {
-    return view('sign-up');
+    return view('auth.sign-up');
 });
 Route::get('/dashboard', function () {
+    // if (Auth::guard('admin')->check()) {
+    //     notify()->success('Halo selamat datang ' . Auth::guard('admin')->user()->name);
+    // }
     return view('dashboard', ['title' => 'Dashboard']);
-});
+})->middleware('auth:admin');
 Route::resource('/guard', GuardController::class);
 
 // Menampilkan daftar jadwal
