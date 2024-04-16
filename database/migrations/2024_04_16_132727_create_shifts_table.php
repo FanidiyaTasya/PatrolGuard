ab628a68eb9510a85d4a99ada9802067e891fd70
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-04-16 21:06:00 +0700

    feat: add migration for shifts entity

--- database/migrations/2024_04_16_132727_create_shifts_table.php
+++ database/migrations/2024_04_16_132727_create_shifts_table.php
@@ -12,8 +12,8 @@ return new class extends Migration {
         Schema::create('shifts', function (Blueprint $table) {
             $table->id();
             $table->string('shift_name');
-            $table->time('start_time');
-            $table->time('end_time');
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
         });
     }
 
