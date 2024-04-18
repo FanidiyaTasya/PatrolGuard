7f18a1140c93a5e29c4ec49a38ccad9ecfb32ccc
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-04-18 15:50:48 +0700

    feat: add edit shift schedules form

---
+++ resources/views/schedule/shift/edit.blade.php
@@ -0,0 +1,46 @@
+<div class="modal fade" id="shiftModalEdit" tabindex="-1" aria-labelledby="shiftModalEditLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="shiftModalEditLabel">Jadwal Shift</h5>
+          <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form method="POST" action="/schedules/shift/{{ $shift->id }}">
+                @method('put')
+                @csrf
+                <input type="hidden" id="shift_id" name="shift_id" value="">
+
+                <div class="w-full max-w-full px-3 shrink-0 md:flex-0">
+                    <div class="mb-4 flex flex-col relative">
+                        <label for="shift_name"
+                            class="inline-block ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Nama Shift</label>
+                        <input type="text" name="shift_name" value="{{ old('shift_name', $shift->shift_name ) }}"
+                            class=" focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+                <div class="w-full max-w-full px-3 shrink-0 md:flex-0">
+                    <div class="mb-4 flex flex-col relative">
+                        <label for="start_time"
+                            class="inline-block ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Waktu Mulai</label>
+                        <input type="time" name="start_time" value="{{ old('start_time', $shift->start_time) }}"
+                            class=" focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+                <div class="w-full max-w-full px-3 shrink-0 md:flex-0">
+                    <div class="mb-4 flex flex-col relative">
+                        <label for="end_time"
+                            class="inline-block ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Waktu Selesai</label>
+                        <input type="time" name="end_time" value="{{ old('end_time', $shift->end_time) }}" 
+                            class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                </div>
+            
+                <div class="modal-footer">
+                    <button type="submit" class="inline-block px-8 py-2 mb-4 font-bold leading-normal text-center text-white align-middle transition-all ease-in bg-tosca border-0 rounded-lg shadow-md cursor-pointer text-xs tracking-tight-rem hover:shadow-xs hover:-translate-y-px active:opacity-85">Simpan</button>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  </div>
