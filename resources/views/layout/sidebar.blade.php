29c3a14e42e7a77573cc1afa4979347434bfb436
FanidiyaTasya <fanidiyatasya@gmail.com>
2024-04-25 18:05:49 +0700

    feat: add feature to sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -50,6 +50,15 @@
         </li>
 
         <li class="mt-0.5 w-full">
+          <a class="{{ Request::is('presence') ? 'bg-blue-500/13 rounded-lg font-semibold text-slate-700' : '' }} dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="/presence">
+            <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
+              <i class="relative top-0 text-sm leading-normal text-yellow-500 ni ni-folder-17"></i>
+            </div>
+            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Presensi</span>
+          </a>
+        </li>
+
+        <li class="mt-0.5 w-full">
           <a class="{{ Request::is('report') ? 'bg-blue-500/13 rounded-lg font-semibold text-slate-700' : '' }} dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors" href="/report">
             <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
               <i class="relative top-0 text-sm leading-normal text-emerald-500 ni ni-collection"></i>
